lukehsiao/git-stats

commit 627d3f09dd596ae862fe3a4de57bcd74d3b94847

feat: allow sorting by other columns
This patch adds the `--sort` option, to determine which column is used
to determine the ordering of the rows.

We also add `--reverse` in case someone wants to reverse the order.

Closes: https://github.com/lukehsiao/git-stats/issues/36

src/main.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
 use anyhow::{bail, Result};
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use rayon::prelude::*;
 use tabled::{
     settings::{
@@ -33,6 +33,28 @@ struct Cli {
     /// Show who reviewed/tested commits based on `Acked-by`, `Tested-by`, and
     /// `Reviewed-by` git trailers.
     reviews: bool,
+    /// What column to sort by
+    #[arg(short, long, value_enum, default_value_t = SortBy::Commits)]
+    sort: SortBy,
+    /// Whether to reverse the sorting from descending to ascending
+    #[arg(long)]
+    reverse: bool,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum SortBy {
+    /// Sort by author alphabetic order
+    Author,
+    /// Sort by number of commits
+    Commits,
+    /// Sort by number of files touched
+    Files,
+    /// Sort by number of insertions
+    Insertions,
+    /// Sort by number of deletions
+    Deletions,
+    /// Sort by net lines of change
+    Net,
 }
 
 #[derive(Tabled)]
@@ -148,6 +170,18 @@ fn main() -> Result<()> {
             })
             .filter_map(|r| r.ok())
             .collect::<_>();
+
+        match cli.sort {
+            SortBy::Author => stats.sort_unstable_by(|a, b| b.author.cmp(&a.author)),
+            SortBy::Commits => stats.sort_unstable_by(|a, b| b.commits.cmp(&a.commits)),
+            SortBy::Files => stats.sort_unstable_by(|a, b| b.num_files.cmp(&a.num_files)),
+            SortBy::Insertions => stats.sort_unstable_by(|a, b| b.insertions.cmp(&a.insertions)),
+            SortBy::Deletions => stats.sort_unstable_by(|a, b| b.deletions.cmp(&a.deletions)),
+            SortBy::Net => stats.sort_unstable_by(|a, b| b.net.cmp(&a.net)),
+        }
+        if cli.reverse {
+            stats.reverse()
+        }
 
         // Collect totals
         let totals = stats.iter().fold(

commit 9766ad42503474ffe80f9f66743bc80232eabb34

perf: drop unneeded derives and sort
It's already sorted by commit by `git`, so we don't need to do anything
in that code path, improving performance.

Similarly, we don't need all of the deleted `derive`s here. They don't
do anything.

Fixes: 627d3f09dd59 ("feat: allow sorting by other columns")

src/main.rs
@@ -41,7 +41,7 @@ struct Cli {
     reverse: bool,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+#[derive(Clone, ValueEnum)]
 enum SortBy {
     /// Sort by author alphabetic order
     Author,
@@ -173,7 +173,7 @@ fn main() -> Result<()> {
 
         match cli.sort {
             SortBy::Author => stats.sort_unstable_by(|a, b| b.author.cmp(&a.author)),
-            SortBy::Commits => stats.sort_unstable_by(|a, b| b.commits.cmp(&a.commits)),
+            SortBy::Commits => (), // It's already sorted by commits
             SortBy::Files => stats.sort_unstable_by(|a, b| b.num_files.cmp(&a.num_files)),
             SortBy::Insertions => stats.sort_unstable_by(|a, b| b.insertions.cmp(&a.insertions)),
             SortBy::Deletions => stats.sort_unstable_by(|a, b| b.deletions.cmp(&a.deletions)),